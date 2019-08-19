91f36fe2f644a848f5f650e6bf4a27663fcb7b33
Taimoor <bitf16m543@pucit.edu.pk>
2019-08-19 21:37:18 +0500

    First commit

---
+++ config/routes.rb
@@ -0,0 +1,12 @@
+Rails.application.routes.draw do
+
+  # get 'welcome/home', to: 'welcome#home'
+  # get 'welcome/about', to: 'welcome#about'
+  #
+  root 'welcome#home'
+  get 'about', to: 'welcome#about'
+
+
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
